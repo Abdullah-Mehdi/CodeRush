8c656fe258dbe91dd572f52f31a5bdd242ceba9e
Michael Asomani <96604042+denkyirahh@users.noreply.github.com>
2024-10-31 11:27:59 -0400

    Create problem.java

---
+++ backend/problems/problem.java
@@ -0,0 +1,8 @@
+public class Problem {
+    // Attributes
+    private String title = "";
+    private String description = "";
+    private String difficulty = "";
+    private int[] testCaseNums = {};
+    
+}
