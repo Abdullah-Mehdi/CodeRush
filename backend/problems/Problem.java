d2af599ed0980efd662ce06c8f6fcc49a5d85c67
Michael Asomani <asomanimichaelma@gmail.com>
2024-11-04 17:52:31 -0500

    added problem files

--- backend/problems/Problem.java
+++ backend/problems/Problem.java
@@ -1,23 +1,34 @@
 public class Problem {
     // Attributes
+    private String id = "";
     private String title = "";
     private String description = "";
     private String difficulty = "";
     private List<Object[]> testCases = {};
     // Constructor
-    public Problem() {}
-    // Setters
+    public Problem(String description, String title, String id, String difficulty, List<Object[]> testCases) {
+        this.description = description;
+        this.title = title;
+        this.id = id;
+        this.difficulty = difficulty;
+        this.testCases = testCases;
+    }
+
+    public void setId(String id) {this.id = id;}
+
     public void setTitle(String title) {this.title = title;}
 
     public void setDescription(String description) {this.description = description;}
 
     public void setDifficulty(String difficulty) {this.difficulty = difficulty;}
 
-    public void setTestCaseNums(List<Object[]> testCases) {this.testCases = testCaseNums;}
+    public void setTestCases(List<Object[]> testCases) {this.testCases = testCases;}
 
     public void setTarget(int target) {this.target = target;}
 
     // Getters
+    public String getId() {return id;}
+
     public String getTitle() {
         return title;
     }
@@ -32,9 +43,5 @@ public class Problem {
 
     public List<Object[]> getTestCases() {
         return testCases;
-    }
-
-    public int getTarget() {
-        return target;
     }
 }
