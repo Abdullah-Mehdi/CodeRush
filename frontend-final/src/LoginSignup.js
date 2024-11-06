3884e96577d5cc1f66fdd066a54533d6228d2bcc
Sabrialinton <Sabrialinton1@gmail.com>
2024-11-05 19:53:12 -0500

    Login Page Update

--- frontend-final/src/LoginSignup.js
+++ frontend-final/src/LoginSignup.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Div, Text, Button, Anchor } from 'atomize';
+import { Div, Text, Button, Anchor, Input, Icon } from 'atomize';
 import { Link } from 'react-router-dom';
 
 function LoginSignup() {
@@ -39,7 +39,7 @@ function LoginSignup() {
             </Div>
 
             {/* Main Content Section */}
-            <Div d="flex" flexDir="column" align="center" justify="center" p="3rem" flexGrow="1">
+            <Div d="flex" flexDir="column" align="center" p="3rem" flexGrow="1" m={{ b: "1rem" }}>
                 {/* Title */}
                 <Text tag="h1" textSize="display2" m={{ b: "1rem" }}>
                     Login or Signup
@@ -51,8 +51,72 @@ function LoginSignup() {
                 </Text>
             </Div>
 
+            {/* Login Form Section */}
+            <Div d="flex" justify="center" align="center" m={{ b: "3rem" }}>
+                <Div p="2rem" bg="white" shadow="4" rounded="lg" w={{ xs: "90%", sm: "20rem" }}>
+
+                    {/* Title */}
+                    <Text tag="h2" textSize="heading" textAlign="center" m={{ b: "1rem" }}>
+                        Login into your account
+                    </Text>
+
+                    {/* Signup Link */}
+                    <Text textSize="body" textAlign="center" textColor="gray600" m={{ b: "2rem" }}>
+                        Don't have an account yet? <Anchor href="/signup" textColor="info700">Create New</Anchor>
+                    </Text>
+
+                    {/* Email Input */}
+                    <Input
+                        placeholder="johndoe@gmail.com"
+                        suffix={
+                            <Icon
+                                name="Email"
+                                color="gray700"
+                                size="16px"
+                                cursor="pointer"
+                                pos="absolute"
+                                top="50%"
+                                right="1rem"
+                                transform="translateY(-50%)"
+                            />
+                        }
+                        m={{ b: "1rem" }}
+                    />
+
+                    {/* Password Input */}
+                    <Input
+                        placeholder="Password"
+                        type="password"
+                        suffix={
+                            <Icon
+                                name="Eye"
+                                color="gray700"
+                                size="16px"
+                                cursor="pointer"
+                                pos="absolute"
+                                top="50%"
+                                right="1rem"
+                                transform="translateY(-50%)"
+                            />
+                        }
+                        m={{ b: "1.5rem" }}
+                    />
+
+                    {/* Login Button */}
+                    <Button
+                        bg="info200"
+                        hoverBg="info300"
+                        textColor="info700"
+                        w="100%"
+                        rounded="circle"
+                    >
+                        Login
+                    </Button>
+                </Div>
+            </Div>
+
             {/* Footer */}
-            <Div bg="gray100" p="3rem" d="flex" justify="space-between">
+            <Div bg="gray100" p="3rem" d="flex" justify="space-between" position="relative" bottom="0" left="0" right="0">
                 <Div d="flex" flexDir="column" align="flex-start" m={{ r: "3rem", b: "2rem" }}>
                     <Text textSize="title" m={{ b: "1rem" }}>Coderush</Text>
                     <Link to="/" style={{ color: 'black', marginBottom: '0.5rem', textDecoration: 'none' }}>Features</Link>
@@ -60,6 +124,7 @@ function LoginSignup() {
                     <Link to="/" style={{ color: 'black', marginBottom: '0.5rem', textDecoration: 'none' }}>Development</Link>
                 </Div>
 
+                {/* Other footer sections */}
                 <Div d="flex" flexDir="column" align="flex-start" m={{ r: "3rem", b: "2rem" }}>
                     <Text textSize="title" m={{ b: "1rem" }}>Resources</Text>
                     <Link to="/" style={{ color: 'black', marginBottom: '0.5rem', textDecoration: 'none' }}>Learn Java</Link>
