c7600c0695caad808636c870e20987e79f6740b0
nick <hermreckn@gmail.com>
2024-11-17 18:31:11 -0500

    Updated tables.sql added sql file to add more test cases for the coding problems. Added method to get testcases in mysqlhandler.

--- backend/src/main/java/com/coderush/mysql/SqlTester.java
+++ backend/src/main/java/com/coderush/mysql/SqlTester.java
@@ -19,23 +19,33 @@ public class SqlTester {
         MySqlHandler.addUser("Reaact","yhwraryhaweryh","awefasd@askaweajdha.sad");
         */
 
-        for(int i = 0; i <3; i++) {
-            System.out.println(MySqlHandler.getProblem(1)[i]);
+//        for(int i = 0; i <3; i++) {
+//            System.out.println(MySqlHandler.getProblem(1)[i]);
+//        }
+//        System.out.println(MySqlHandler.getStartingCode(1));
+//        //MySqlHandler.addSolo("Tommy",1,0);
+//
+//        int numRows = 50;
+//
+//        // Get the leaderboard
+//        ArrayList<ArrayList<String>> leaderboard = MySqlHandler.getLeaderboard(numRows);
+//
+//        // Print out the results
+//        System.out.println("Leaderboard:");
+//        for (List<String> row : leaderboard) {
+//            String username = row.get(0); // username at index 0
+//            String score = row.get(1); // score at index 1
+//            System.out.println(username + " - " + score);
+//        }
+
+        ArrayList<ArrayList<String>> testCases = MySqlHandler.getTestCases(9);
+
+        for(ArrayList<String> test : testCases){
+            for(String str : test){
+                System.out.print(str + " ");
+            }
+            System.out.println();
         }
-        System.out.println(MySqlHandler.getStartingCode(1));
-        //MySqlHandler.addSolo("Tommy",1,0);
 
-        int numRows = 50;
-
-        // Get the leaderboard
-        ArrayList<ArrayList<String>> leaderboard = MySqlHandler.getLeaderboard(numRows);
-
-        // Print out the results
-        System.out.println("Leaderboard:");
-        for (List<String> row : leaderboard) {
-            String username = row.get(0); // username at index 0
-            String score = row.get(1); // score at index 1
-            System.out.println(username + " - " + score);
-        }
     }
 }
---
+++ backend/src/main/java/com/coderush/mysql/testcases.sql
@@ -0,0 +1,14 @@
+USE justwork;
+
+
+--INSERT INTO testcases (problemid, input, output) VALUES ('problemid', 'input', 'output');
+INSERT INTO testcases (problemid, input, output) VALUES ('9', '1', 'true');
+INSERT INTO testcases (problemid, input, output) VALUES ('9', '121', 'true');
+INSERT INTO testcases (problemid, input, output) VALUES ('9', '1211', 'false');
+INSERT INTO testcases (problemid, input, output) VALUES ('5', 'Nobody', 'obo');
+INSERT INTO testcases (problemid, input, output) VALUES ('5', 'for loops', 'oo');
+INSERT INTO testcases (problemid, input, output) VALUES ('5', 'Theres a lost dog', 'ere');
+--taken from random wikipedia articles
+INSERT INTO testcases (problemid, input, output) VALUES ('3', 'The Atkins district, and therefore the attendance boundary of the high school,', 'The Atkins district, and therefore the at');
+INSERT INTO testcases (problemid, input, output) VALUES ('3', 'In January 2018, his Anzhi contract was dissolved following his arrest in the autumn of 2017 in Belgium.', 'In January 2018, his Anzhi contract was dis');
+INSERT INTO testcases (problemid, input, output) VALUES ('3', 'Bonchester cheese is a soft Scottish cheese, made from unpasteurized Jersey cows milk.', 'Bonchester che');
