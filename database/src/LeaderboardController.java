a8e8d94d6425237e60ca5ee38b24753b7e867122
Sabrialinton <Sabrialinton1@gmail.com>
2024-11-18 13:31:59 -0500

    Creation of LeaderboardController.java

---
+++ database/src/LeaderboardController.java
@@ -0,0 +1,23 @@
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api")
+public class LeaderboardController {
+
+    @GetMapping("/leaderboard")
+    public List<Map<String, String>> getLeaderboard(@RequestParam(defaultValue = "10") int top) {
+        ArrayList<ArrayList<String>> leaderboardData = MySqlHandler.getLeaderboard(top);
+        List<Map<String, String>> response = new ArrayList<>();
+
+        if (leaderboardData != null) {
+            for (ArrayList<String> user : leaderboardData) {
+                Map<String, String> userInfo = new HashMap<>();
+                userInfo.put("username", user.get(0));
+                userInfo.put("totalscore", user.get(1));
+                response.add(userInfo);
+            }
+        }
+        return response;
+    }
+}
