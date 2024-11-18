ef093c1a4b6aacf5276fa80b07e41980a1b9a335
BlankIts <77278734+Abdullah-Mehdi@users.noreply.github.com>
2024-11-18 07:30:53 -0500

    Emoji fix, removal of Helmet to cut down dependencies

--- frontend-final/src/App.js
+++ frontend-final/src/App.js
@@ -1,28 +1,28 @@
-import React from 'react'; // Importing React library for building components
-import { Helmet } from 'react-helmet'; // Importing Helmet for managing document head
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importing Router components for navigation
-import LandingPage from './LandingPage'; // Landing page component
-import ProblemLibrary from './ProblemLibrary'; // Problem library component
-import PracticeMode from './PracticeMode'; // Practice mode component
-import Duel from './Duel'; // Duel mode component
-import LoginSignup from './LoginSignup'; // Login/Signup component
-import './index.css'; // Importing global CSS styles
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './LandingPage';
+import ProblemLibrary from './ProblemLibrary';
+import PracticeMode from './PracticeMode';
+import Duel from './Duel';
+import LoginSignup from './LoginSignup';
+import './index.css';
 
 function App() {
+  useEffect(() => {
+    document.title = '\u{1F4BB} CodeRush';  // Unicode for computer emoji
+  }, []);
+
   return (
-    <Router> {/* Wrap the app in a Router for navigation */}
-        <Helmet>
-        <title>CodeRush</title>
-        </Helmet>
-      <Routes> {/* Define all app routes */}
-        <Route path="/" element={<LandingPage />} /> {/* Home/Landing page route */}
-        <Route path="/problem-library" element={<ProblemLibrary />} /> {/* Problem Library route */}
-        <Route path="/practice-mode/:id" element={<PracticeMode />} /> {/* Practice Mode route with dynamic ID */}
-        <Route path="/duel/:id" element={<Duel />} /> {/* Duel Mode route with dynamic ID */}
-        <Route path="/login-signup" element={<LoginSignup />} /> {/* Login and Signup page route */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/problem-library" element={<ProblemLibrary />} />
+        <Route path="/practice-mode/:id" element={<PracticeMode />} />
+        <Route path="/duel/:id" element={<Duel />} />
+        <Route path="/login-signup" element={<LoginSignup />} />
       </Routes>
     </Router>
   );
 }
 
-export default App; // Export App component as default
+export default App;
